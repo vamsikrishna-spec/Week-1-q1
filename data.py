f97b4759c3184b1c47886ccf5eb160e20cbbbe3b
vamsikrishna-spec <24331a05e7@mvgrce.edu.in>
2025-12-16 13:41:14 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter number 1: "))
+print("Value of number1:", num1)
+char = 'a'
+print("Character is:", char)
+num2 = float(input("Enter number 2: "))
+print("Float value:", num2)
+string_val = "vamsi"
+print("String is:", string_val)
